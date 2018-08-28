6e78c908fe3ae2eb7d6517dc926ce5d2789945c0
mtaddevi <mtaddevi@gmail.com>
2018-08-28 14:55:57 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//Basic hello world produced for CS180 lab1 at Purdue University
+//@Author Michael Taddeucci
+
+public class Hello {
+  public static void main(String[ ] args) {
+    System.out.println("Hello, world!");
+  }
+}
